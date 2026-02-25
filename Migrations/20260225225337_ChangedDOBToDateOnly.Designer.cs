55b10fa3ac260d54eb0cec94c6f59a735e4da631
mchena <m.chena@itecriocuarto.org.ar>
2026-02-25 20:01:26 -0300

    DateOfBirth datatype change

--- Migrations/20260225225337_ChangedDOBToDateOnly.Designer.cs
+++ Migrations/20260225225337_ChangedDOBToDateOnly.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MedicalClinicAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MedicalClinicApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260225225337_ChangedDOBToDateOnly")]
+    partial class ChangedDOBToDateOnly
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
