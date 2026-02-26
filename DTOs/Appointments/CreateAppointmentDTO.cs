9755b11d74a61f8cf007ceddf9b1f3498c0f62b4
mchena <m.chena@itecriocuarto.org.ar>
2026-02-26 20:46:23 -0300

    DTO for create an appointment

---
+++ DTOs/Appointments/CreateAppointmentDTO.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MedicalClinicAPI.DTOs.Appointments
+{
+    public class CreateAppointmentDTO
+    {
+        [Required]
+        public int? DoctorId { get; set; }
+
+        public int? PatientId { get; set; }
+
+        [Required]
+        public DateTime AppointmentDate { get; set; }
+    }
+};
