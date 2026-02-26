e4605808077edb18d6dad67cbee18ac44931e324
mchena <m.chena@itecriocuarto.org.ar>
2026-02-25 21:29:30 -0300

    .

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MedicalClinicAPI.Controllers;
 
