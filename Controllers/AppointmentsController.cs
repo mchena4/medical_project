b26e12e7cde15651ac03152c0fc9b298ed0d70c1
mchena <m.chena@itecriocuarto.org.ar>
2026-03-01 18:27:10 -0300

    Refactoring

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 using MedicalClinicAPI.Data;
 using MedicalClinicAPI.Models;
 using MedicalClinicAPI.DTOs.Appointments;
