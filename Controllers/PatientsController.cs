a3c6e862616e139955c40dff2460066756aaed39
mchena <m.chena@itecriocuarto.org.ar>
2026-03-02 19:58:18 -0300

    Create a patient from the inside

--- Controllers/PatientsController.cs
+++ Controllers/PatientsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MedicalClinicAPI.Data;
 using MedicalClinicAPI.DTOs.Patients;
+using MedicalClinicAPI.Models;
 
 namespace MedicalClinicalAPI.Controllers;
 
@@ -58,5 +59,54 @@ public class PatientsController : ControllerBase
         patient.DateOfBirth = request.DateOfBirth ?? patient.DateOfBirth;
         await _context.SaveChangesAsync();
         return Ok("Patient updated successfully.");
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Receptionist")]
+    // This endpoint allows receptionists to create new patients.
+    public async Task<IActionResult> CreatePatient(CreatePatientDTO request)
+    {
+        // Get the patient role from the database
+        var patientRole = await _context.Roles.FirstOrDefaultAsync(r => r.Name == "Patient");
+        if (patientRole == null) return StatusCode(500, "Patient role not found in the database.");
+
+        // Check if a patient with the same DNI or email already exists
+        if(await _context.Patients.AnyAsync(p => p.Dni == request.Dni))
+        {
+            return BadRequest("A patient with the same DNI already exists.");
+        }
+
+        if (await _context.Users.AnyAsync(u => u.Email == request.Email))
+        {
+            return BadRequest("This email is already in use.");
+        }
+
+        // Create a new user for the patient
+        var newUser = new User 
+        {
+            Email = request.Email,
+            Password = BCrypt.Net.BCrypt.HashPassword(request.Dni), // Using DNI as the initial password, but it should be changed by the user later.
+            RoleId = patientRole.Id
+        };
+
+        _context.Users.Add(newUser);
+        await _context.SaveChangesAsync();
+
+
+        // Create a new patient linked to the user
+        var patient = new Patient
+        {
+            UserId = newUser.Id,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Dni = request.Dni,
+            DateOfBirth = request.DateOfBirth,
+            PhoneNumber = request.PhoneNumber ?? "N/A",        
+        };
+
+        _context.Patients.Add(patient);
+        await _context.SaveChangesAsync();
+
+        return Ok("Patient created successfully.");
     }
 }
