dfd16e3062c762adc76965d9f71980745ac44384
mchena <m.chena@itecriocuarto.org.ar>
2026-07-30 02:22:46 -0300

    Rate limiter config added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using MedicalClinicAPI.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
@@ -31,12 +33,61 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+
+// Rate Limiting Config
+builder.Services.AddRateLimiter( options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+    // Response and log config 
+    options.OnRejected = async (context, token) =>
+    {
+        // Get the logger and data
+        var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<Program>>();
+        var ip = context.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown IP address";
+        var route = context.HttpContext.Request.Path;
+
+        // Register logger
+        logger.LogWarning("Rate Limit exceeded by {Ip}. {Route} access attempt", ip, route);
+
+        // HTTP response
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        context.HttpContext.Response.ContentType = "application/json";
+
+        string ? retryAfterMessage = null;
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            context.HttpContext.Response.Headers["Retry-After"] = ((int)retryAfter.TotalSeconds).ToString();
+            retryAfterMessage = $"Please, try again in {(int)retryAfter.TotalSeconds} seconds.";
+        }
+
+        // Json for message response
+        var responseJson = new
+        {
+            error = "Too Many Requests",
+            messagge = "You have exceeded the requests limit",
+            detail = retryAfterMessage ?? "Please try again later."
+        };
+
+        await context.HttpContext.Response.WriteAsJsonAsync(responseJson, cancellationToken: token);
+    };
+
+    // Limit politics 
+    options.AddFixedWindowLimiter(policyName: "BasicPolitics", limiterOptions =>
+    {
+        limiterOptions.PermitLimit = 5;
+        limiterOptions.Window = TimeSpan.FromSeconds(10);
+        limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        limiterOptions.QueueLimit = 0;
+    });
+
+});
+
 // CORS Configuration
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowNextJs", policy =>
     {
-        // Lee los orígenes permitidos separados por coma desde la configuración
         var origins = builder.Configuration.GetValue<string>("Cors:Origins")?.Split(',');
         
         if (origins != null && origins.Length > 0)
@@ -66,6 +117,9 @@ app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseRateLimiter();
+
 app.MapControllers();
 
 app.Run();
